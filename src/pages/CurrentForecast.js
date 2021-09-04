3a51d7fc68aea1311625cdd0dbc7c633054667cd
Rasmus Eldblad <r.eldblad@protonmail.com>
2021-09-04 18:47:14 +0200

    This commits adds capability to render current city

--- src/pages/CurrentForecast.js
+++ src/pages/CurrentForecast.js
@@ -2,33 +2,41 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 function CurrentForecastPage() {
-	let url;
+	let weatherUrl;
+	let geolocationUrl;
 	const [weather, setWeather] = useState(null);
+	const [geolocation, setGeolocation] = useState(null);
 	useEffect(() => {
 		if ("geolocation" in navigator) {
 			navigator.geolocation.getCurrentPosition(function (position) {
 				const usersLatitude = position.coords.latitude;
 				const usersLongitude = position.coords.longitude;
-				const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
+				const openWeatherKey = process.env.REACT_APP_WEATHER_API_KEY;
+				const geolocationKey = process.env.REACT_APP_GOOGLE_GEOLOCATION_API_KEY;
 				const metric = "metric";
-				url = `https://api.openweathermap.org/data/2.5/onecall?lat=${usersLatitude}&lon=${usersLongitude}&units=${metric}&appid=${apiKey}`;
-				axios.get(url).then((response) => {
+				weatherUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${usersLatitude}&lon=${usersLongitude}&units=${metric}&appid=${openWeatherKey}`;
+				geolocationUrl = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${usersLatitude},${usersLongitude}&key=${geolocationKey}`;
+				axios.get(weatherUrl).then((response) => {
 					setWeather(response.data);
+				});
+				axios.get(geolocationUrl).then((response) => {
+					setGeolocation(response.data);
 				});
 			});
 		} else {
 			console.log("Geolocation is disabled");
 		}
-	}, [url]);
+	}, [weatherUrl, geolocationUrl]);
 
 	console.log(weather);
+	console.log(geolocation);
 
-	if (weather)
+	if (weather && geolocation)
 		return (
 			<div>
 				<section className="container mx-auto p-5 text-center sm:text-left">
 					<h1 className="sm:text-5xl mx:text-3x1 font-sans font-bold">
-						{weather.timezone}
+						{geolocation.results[5].formatted_address}
 					</h1>
 					<h2 className="mx:text-4x1 font-sans font-semibold">
 						{weather.current.temp}&#8451;
